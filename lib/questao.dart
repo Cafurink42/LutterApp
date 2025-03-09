b1c8afbf16f063ecfe436f27446eb65927859723
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2025-03-09 20:20:53 -0300

    Estilização do componente Questao

--- lib/questao.dart
+++ lib/questao.dart
@@ -7,6 +7,25 @@ class Questao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(10), // margin 10px em todos os cantos
+        child: Text(
+          texto,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
+
+
+//criar um componente chamado resposta.dart
+//criar um componente stateles dentro do resposta.dart
+//criar um construtor que irá receber um texto que será o valor que irá colocar dentro do botão
+/**
+ passar o componente criado como parâmetro para esse componente:
+ * ElevatedButton(
+              child: Text('Resposta 1'), //mudar aqui 
+              onPressed: _responder, //método responder
+            ),
+ */
